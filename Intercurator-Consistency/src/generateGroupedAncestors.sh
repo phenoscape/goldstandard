ede20dcb31801f185d5f78a08755a3f3e14253d6
Hilmar Lapp <hlapp@drycafe.net>
2018-04-11 12:29:06 -0400

    Changes paths, adds instructions to fit into Zenodo deposit

--- Intercurator-Consistency/src/generateGroupedAncestors.sh
+++ Intercurator-Consistency/src/generateGroupedAncestors.sh
@@ -1,93 +1,100 @@
+# also ensure to set CLASSPATH to include the Java dependencies
+JAVAC=javac
+JAVA=java
+
+# Either put MySQL client into the path, or set full path here.
+# Also, include login credentials if any are needed to connect with full
+# DML privileges.
+MYSQL=mysql
+
+# where the data and ontologies directories are
+DATADIR=../InterCuratorData
+ONTOLOGIESDIR=../InterCuratorOntologies
+
+##############################################################################
+# -- no more customizations below except for MySQL LOAD DATA INFILE below -- #
+##############################################################################
+
 # Add relations classes
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/AddRelationClasses.java
-java -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/AddRelationClasses
+$JAVAC  ./reasoning/AddRelationClasses.java
+$JAVA ./reasoning/AddRelationClasses
 
 # Querying for superclasses using the Composite ontology after adding relations classes
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/KR--AD_40718.tsv ../../data/Ancestors40718.txt
+javac  ./reasoning/populateAncestorsAll.java
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/KR--AD_40718.tsv $DATADIR/Ancestors40718.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/KR--NI_40716.tsv $DATADIR/Ancestors40716.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/KR--NI_40716.tsv ../../data/Ancestors40716.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/KR--WD_40717.tsv $DATADIR/Ancestors40717.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/KR--WD_40717.tsv ../../data/Ancestors40717.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/NR--WD_38484.tsv $DATADIR/Ancestors38484.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/NR--WD_38484.tsv ../../data/Ancestors38484.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/NR--AD_40674.tsv $DATADIR/Ancestors40674.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/NR--AD_40674.tsv ../../data/Ancestors40674.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/NR--NI_40676.tsv $DATADIR/Ancestors40676.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/NR--NI_40676.tsv ../../data/Ancestors40676.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/Transformed_CP_best.tsv $DATADIR/AncestorsCP_AllBest.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/Transformed_CP_best.tsv ../../data/AncestorsCP_AllBest.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/Transformed_CP_InitialOntologies.tsv $DATADIR/InitialOntologiesAncestors.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/Transformed_CP_InitialOntologies.tsv ../../data/InitialOntologiesAncestors.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/Transformed_KR--CP_40716.tsv $DATADIR/AncestorsCP_40716.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/Transformed_KR--CP_40716.tsv ../../data/AncestorsCP_40716.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/Transformed_KR--CP_40717.tsv $DATADIR/AncestorsCP_40717.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/Transformed_KR--CP_40717.tsv ../../data/AncestorsCP_40717.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/Transformed_KR--CP_40718.tsv $DATADIR/AncestorsCP_40718.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/Transformed_KR--CP_40718.tsv ../../data/AncestorsCP_40718.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/Transformed_NR--CP_38484.tsv $DATADIR/AncestorsCP_38484.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/Transformed_NR--CP_38484.tsv ../../data/AncestorsCP_38484.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/Transformed_NR--CP_40674.tsv $DATADIR/AncestorsCP_40674.txt
 
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/Transformed_NR--CP_40674.tsv ../../data/AncestorsCP_40674.txt
-
-javac -cp  ".:../../data/owltools-runner-all.jar" ./reasoning/populateAncestorsAll.java
-java  -cp  ".:../../data/owltools-runner-all.jar" -Xmx60G reasoning.populateAncestorsAll  ../../data/Transformed_NR--CP_40676.tsv ../../data/AncestorsCP_40676.txt
+$JAVA -Xmx60G reasoning.populateAncestorsAll  $DATADIR/Transformed_NR--CP_40676.tsv $DATADIR/AncestorsCP_40676.txt
 
 # Creating a comprehensive Superclass file for all annotations
-cat ../../data/AncestorsCP_40676.txt ../../data/AncestorsCP_40674.txt ../../data/AncestorsCP_38484.txt ../../data/AncestorsCP_40718.txt ../../data/AncestorsCP_40717.txt ./../data/AncestorsCP_40716.txt ../../data/InitialOntologiesAncestors.txt ../../data/AncestorsCP_AllBest.txt ../../data/Ancestors40676.txt ../../data/Ancestors40674.txt ../../data/Ancestors38484.txt ../../data/Ancestors40716.txt ../../data/Ancestors40717.txt ../../data/Ancestors40718.txt > ../../data/AllAncestorsCurationExperiment.txt
+cat $DATADIR/AncestorsCP_40676.txt $DATADIR/AncestorsCP_40674.txt $DATADIR/AncestorsCP_38484.txt $DATADIR/AncestorsCP_40718.txt $DATADIR/AncestorsCP_40717.txt $DATADIR/AncestorsCP_40716.txt $DATADIR/InitialOntologiesAncestors.txt $DATADIR/AncestorsCP_AllBest.txt $DATADIR/Ancestors40676.txt $DATADIR/Ancestors40674.txt $DATADIR/Ancestors38484.txt $DATADIR/Ancestors40716.txt $DATADIR/Ancestors40717.txt $DATADIR/Ancestors40718.txt > $DATADIR/AllAncestorsCurationExperiment.txt
 
-#!/bin/bash
-cp ../../data/AllAncestorsCurationExperiment.txt /usr/local/mysql-5.6.10-osx10.7-x86_64/data/ontologies/AllAncestorsCurationExperiment.txt
+# This is so we have a hard-codable location for the data file to be loaded
+# by mysql below. The assumption here is that the server allows LOCAL INFILE.
+# If it doesn't, remove the LOCAL below. If the server is remote, you will
+# also need to copy the file instead of /tmp to a directory that the server
+# has access to, and then change the path in the LOAD DATA INFILE statement.
+cp -p $DATADIR/AllAncestorsCurationExperiment.txt /tmp/AllAncestorsCurationExperiment.txt
 
 # Creating and loading superclasses into a database
-mysql << EOF
+$MYSQL << EOF
 
 use ontologies;
 
 drop table tbl_allancestorscurationexperiment;
 CREATE TABLE IF NOT EXISTS tbl_allancestorscurationexperiment (   id int(100) NOT NULL AUTO_INCREMENT,   term varchar(700) DEFAULT NULL,   ancestor varchar(700) DEFAULT NULL,   PRIMARY KEY (id),   UNIQUE KEY uniq (term,ancestor) ) ENGINE=InnoDB AUTO_INCREMENT=1706354 DEFAULT CHARSET=latin1;
 
-LOAD DATA INFILE 'AllAncestorsCurationExperiment.txt' IGNORE INTO TABLE tbl_allancestorscurationexperiment FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' (term, ancestor);
+LOAD DATA LOCAL INFILE '/tmp/AllAncestorsCurationExperiment.txt' IGNORE INTO TABLE tbl_allancestorscurationexperiment FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' (term, ancestor);
 
 EOF
 
 
-rm ../../data/AllAncestors_Combinations.txt
+rm $DATADIR/AllAncestors_Combinations.txt
 
-python transform.py ../data/NR--CP_40674.tsv
-python transform.py ../data/NR--CP_40676.tsv
-python transform.py ../data/NR--CP_38484.tsv
-python transform.py ../data/KR--CP_40716.tsv
-python transform.py ../data/KR--CP_40717.tsv
-python transform.py ../data/KR--CP_40718.tsv
-python transform.py ../data/CP_best.tsv
+python transform.py $DATADIR/NR--CP_40674.tsv
+python transform.py $DATADIR/NR--CP_40676.tsv
+python transform.py $DATADIR/NR--CP_38484.tsv
+python transform.py $DATADIR/KR--CP_40716.tsv
+python transform.py $DATADIR/KR--CP_40717.tsv
+python transform.py $DATADIR/KR--CP_40718.tsv
+python transform.py $DATADIR/CP_best.tsv
 
 # Creating grouped EQ subsumers for annotations
-python populategroupedancestors.py ../data/NR--WD_38484.tsv 1 tbl_allancestorscurationexperiment C_EQ_
-python populategroupedancestors.py ../data/NR--AD_40674.tsv 1 tbl_allancestorscurationexperiment C_EQ_
-python populategroupedancestors.py ../data/NR--NI_40676.tsv 1 tbl_allancestorscurationexperiment C_EQ_
-python populategroupedancestors.py ../data/KR--NI_40716.tsv 1 tbl_allancestorscurationexperiment C_EQ_
-python populategroupedancestors.py ../data/KR--WD_40717.tsv 1 tbl_allancestorscurationexperiment C_EQ_
-python populategroupedancestors.py ../data/KR--AD_40718.tsv 1 tbl_allancestorscurationexperiment C_EQ_
+python populategroupedancestors.py $DATADIR/NR--WD_38484.tsv 1 tbl_allancestorscurationexperiment C_EQ_
+python populategroupedancestors.py $DATADIR/NR--AD_40674.tsv 1 tbl_allancestorscurationexperiment C_EQ_
+python populategroupedancestors.py $DATADIR/NR--NI_40676.tsv 1 tbl_allancestorscurationexperiment C_EQ_
+python populategroupedancestors.py $DATADIR/KR--NI_40716.tsv 1 tbl_allancestorscurationexperiment C_EQ_
+python populategroupedancestors.py $DATADIR/KR--WD_40717.tsv 1 tbl_allancestorscurationexperiment C_EQ_
+python populategroupedancestors.py $DATADIR/KR--AD_40718.tsv 1 tbl_allancestorscurationexperiment C_EQ_
 
-python populategroupedancestors.py ../data/Transformed_NR--CP_40674.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
-python populategroupedancestors.py ../data/Transformed_NR--CP_40676.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
-python populategroupedancestors.py ../data/Transformed_NR--CP_38484.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
-python populategroupedancestors.py ../data/Transformed_KR--CP_40716.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
-python populategroupedancestors.py ../data/Transformed_KR--CP_40717.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
-python populategroupedancestors.py ../data/Transformed_KR--CP_40718.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
-python populategroupedancestors.py ../data/Transformed_CP_best.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
-python populategroupedancestors.py ../data/Transformed_CP2012_Biocreative.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
-python populategroupedancestors.py ../data/Transformed_CP_InitialOntologies.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
+python populategroupedancestors.py $DATADIR/Transformed_NR--CP_40674.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
+python populategroupedancestors.py $DATADIR/Transformed_NR--CP_40676.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
+python populategroupedancestors.py $DATADIR/Transformed_NR--CP_38484.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
+python populategroupedancestors.py $DATADIR/Transformed_KR--CP_40716.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
+python populategroupedancestors.py $DATADIR/Transformed_KR--CP_40717.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
+python populategroupedancestors.py $DATADIR/Transformed_KR--CP_40718.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
+python populategroupedancestors.py $DATADIR/Transformed_CP_best.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
+python populategroupedancestors.py $DATADIR/Transformed_CP2012_Biocreative.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
+python populategroupedancestors.py $DATADIR/Transformed_CP_InitialOntologies.tsv 1 tbl_allancestorscurationexperiment CP_EQ_
--- src/generateGroupedAncestors.sh
+++ src/generateGroupedAncestors.sh
@@ -1,51 +1,77 @@
+# This should come with installing the dependency OWLTools
+# (https://github.com/owlcollab/owltools)
+OWLTOOLS=/path/to/OWLTools-Runner/bin/owltools
+
+# also ensure to set CLASSPATH to include the Java dependencies
+JAVAC=javac
+JAVA=java
+
+# Either put MySQL client into the path, or set full path here.
+# Also, include login credentials if any are needed to connect with full
+# DML privileges.
+MYSQL=mysql
+
+# where the data and ontologies directories are
+DATADIR=../GoldStandardData
+ONTOLOGIESDIR=../GoldStandardOntologies
+
+##############################################################################
+# -- no more customizations below except for MySQL LOAD DATA INFILE below -- #
+##############################################################################
+
 # Map Uberon temp IDs to permanent IDs
-python convertUBERONTEMP.py > ../data/UnaccountedTerms.txt
+python convertUBERONTEMP.py > $DATADIR/UnaccountedTerms.txt
 
 # Creating the composite ontology that includes Uberon, PATO, BSPO, and GO
-../data/OWLTools-2015/owltools-read-only/OWLTools-Runner/bin/owltools ../Ontologies/BestMerged.owl ../Ontologies/uberon.owl ../Ontologies/go.owl ../Ontologies/pato-simple.owl ../Ontologies/bspo.owl --merge-support-ontologies -o ../Ontologies/MergedOntology_GS.owl
+$OWLTOOLS $ONTOLOGIESDIR/BestMerged.owl $ONTOLOGIESDIR/uberon.owl $ONTOLOGIESDIR/go.owl $ONTOLOGIESDIR/pato-simple.owl $ONTOLOGIESDIR/bspo.owl --merge-support-ontologies -o $ONTOLOGIESDIR/MergedOntology_GS.owl
 
 
 # Adding relations classes to the composite ontology
-javac -cp  ".:../data/OWLTools-2015/owltools-read-only/OWLTools-Runner/bin/owltools-runner-all.jar" ./reasoning/AddRelationsClasses.java
+$JAVAC ./reasoning/AddRelationsClasses.java
 
-java  -cp  ".:../data/OWLTools-2015/owltools-read-only/OWLTools-Runner/bin/owltools-runner-all.jar" -Xmx60G reasoning.AddRelationsClasses ../data/MergedOntology_GS.owl ../data/MergedOntology_GS_Relations.owl
+$JAVA -Xmx60G reasoning.AddRelationsClasses $DATADIR/MergedOntology_GS.owl $DATADIR/MergedOntology_GS_Relations.owl
 
 # Querying for superclasses of all classes in the Merged ontology with relations classes
-javac -cp  ".:../data/org.semanticweb.owl.owlapi-4.1.jar:../data/elk-owlapi.jar:../data/log4j-1.2.17.jar" ./reasoning/GetAncestors.java
+$JAVAC ./reasoning/GetAncestors.java
 
-java  -cp  ".:../data/org.semanticweb.owl.owlapi-4.1.jar:../data/elk-owlapi.jar:../data/log4j-1.2.17.jar" -Xmx80G reasoning.GetAncestors
+$JAVA -Xmx80G reasoning.GetAncestors
 
 
-cat ../data/MappedAnnotations/*tsv > ../data/AllAnnotations.tsv
+cat $DATADIR/MappedAnnotations/*tsv > $DATADIR/AllAnnotations.tsv
 
-cp ../data/AnnotationSubsumers_Relations.txt /usr/local/mysql-5.6.10-osx10.7-x86_64/data/ontologies/AnnotationSubsumers_Relations.txt
+# This is so we have a hard-codable location for the data file to be loaded
+# by mysql below. The assumption here is that the server allows LOCAL INFILE.
+# If it doesn't, remove the LOCAL below. If the server is remote, you will
+# also need to copy the file instead of /tmp to a directory that the server
+# has access to, and then change the path in the LOAD DATA INFILE statement.
+cp -p $DATADIR/AnnotationSubsumers_Relations.txt /tmp
 
-mysql << EOF
+$MYSQL << EOF
 use ontologies;
 drop table tbl_goldstandardanalysis;
 
 CREATE TABLE IF NOT EXISTS tbl_goldstandardanalysis (   id int(100) NOT NULL AUTO_INCREMENT,   term varchar(700) DEFAULT NULL,   ancestor varchar(700) DEFAULT NULL,   PRIMARY KEY (id),   UNIQUE KEY uniq (term,ancestor) ) ENGINE=InnoDB AUTO_INCREMENT=1706354 DEFAULT CHARSET=latin1;
 
 
-LOAD DATA INFILE 'AnnotationSubsumers_Relations.txt' IGNORE INTO TABLE tbl_goldstandardanalysis FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' (term, ancestor);
+LOAD DATA LOCAL INFILE '/tmp/AnnotationSubsumers_Relations.txt' IGNORE INTO TABLE tbl_goldstandardanalysis FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' (term, ancestor);
 
 EOF
 
 
-rm ../data/AllAncestors_Combinations.txt
+rm $DATADIR/AllAncestors_Combinations.txt
 
 # Populate grouped ancestors for all annotation files
-python populategroupedancestors.py ../data/MappedAnnotations/NR--WD_38484.tsv 1 tbl_goldstandardanalysis C_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/NR--AD_40674.tsv 1 tbl_goldstandardanalysis C_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/NR--NI_40676.tsv 1 tbl_goldstandardanalysis C_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/KR--NI_40716.tsv 1 tbl_goldstandardanalysis C_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/KR--WD_40717.tsv 1 tbl_goldstandardanalysis C_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/KR--AD_40718.tsv 1 tbl_goldstandardanalysis C_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/GS_Dataset.tsv 1 tbl_goldstandardanalysis C_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/Transformed_CP_best.tsv 1 tbl_goldstandardanalysis CP_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/Transformed_NR--CP_38484.tsv 1 tbl_goldstandardanalysis CP_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/Transformed_NR--CP_40674.tsv 1 tbl_goldstandardanalysis CP_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/Transformed_NR--CP_40676.tsv 1 tbl_goldstandardanalysis CP_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/Transformed_KR--CP_40716.tsv 1 tbl_goldstandardanalysis CP_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/Transformed_KR--CP_40717.tsv 1 tbl_goldstandardanalysis CP_EQ_
-python populategroupedancestors.py ../data/MappedAnnotations/Transformed_KR--CP_40718.tsv 1 tbl_goldstandardanalysis CP_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/NR--WD_38484.tsv 1 tbl_goldstandardanalysis C_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/NR--AD_40674.tsv 1 tbl_goldstandardanalysis C_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/NR--NI_40676.tsv 1 tbl_goldstandardanalysis C_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/KR--NI_40716.tsv 1 tbl_goldstandardanalysis C_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/KR--WD_40717.tsv 1 tbl_goldstandardanalysis C_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/KR--AD_40718.tsv 1 tbl_goldstandardanalysis C_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/GS_Dataset.tsv 1 tbl_goldstandardanalysis C_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/Transformed_CP_best.tsv 1 tbl_goldstandardanalysis CP_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/Transformed_NR--CP_38484.tsv 1 tbl_goldstandardanalysis CP_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/Transformed_NR--CP_40674.tsv 1 tbl_goldstandardanalysis CP_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/Transformed_NR--CP_40676.tsv 1 tbl_goldstandardanalysis CP_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/Transformed_KR--CP_40716.tsv 1 tbl_goldstandardanalysis CP_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/Transformed_KR--CP_40717.tsv 1 tbl_goldstandardanalysis CP_EQ_
+python populategroupedancestors.py $DATADIR/MappedAnnotations/Transformed_KR--CP_40718.tsv 1 tbl_goldstandardanalysis CP_EQ_
